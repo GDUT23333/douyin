6d8be08d64bd888b3f86284ec2ec8077f6cb4abc
Ember <maijh2@yonyou.com>
2022-05-10 22:15:12 +0800

    feat:video模块主函数进行rpc注册

--- video_module/go.mod
+++ video_module/go.mod
@@ -1,6 +1,7 @@
 module video_module
 
 go 1.18
+
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.7
@@ -26,8 +27,13 @@ require (
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
+
+	//rpc
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- video_module/main.go
+++ video_module/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"net/http"
+
+	"strings"
 	"sync"
 	"video_module/dao"
 	"video_module/controller"
@@ -22,13 +28,37 @@ func main(){
 		waitGroup.Done()
 	}()
 	r := gin.Default()
+	grpcServer := grpc.NewServer()
 	go func() {
 		InitRouter(r)
+		// 判断协议是否为http/2
+		// 判断是否是grpc
+		r.Use(func(ctx *gin.Context) {
+			// 判断协议是否为http/2
+			// 判断是否是grpc
+			if ctx.Request.ProtoMajor == 2 &&
+				strings.HasPrefix(ctx.GetHeader("Content-Type"), "application/grpc") {
+				// 按grpc方式来请求
+				grpcServer.ServeHTTP(ctx.Writer, ctx.Request)
+				// 不要再往下请求了,防止继续链式调用拦截器
+				ctx.Abort()
+				return
+			}
+			// 当作普通api
+			ctx.Next()
+		})
 		waitGroup.Done()
 	}()
 	waitGroup.Wait()
-	//start
-	r.Run()
+	// 为http/2配置参数
+	h2Handle := h2c.NewHandler(r, &http2.Server{}) // 禁用TLS加密协议
+	// 配置http服务
+	server := &http.Server{
+		Addr: ":8081",
+		Handler: h2Handle,
+	}
+	// 启动http服务
+	server.ListenAndServe()
 }
 
 //初始化路由
