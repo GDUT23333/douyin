314c47346740862dd151601d408ed372ea861b0b
Ember <maijh2@yonyou.com>
2022-05-10 22:33:58 +0800

    feat:主函数进行rpc注册服务，对外暴露获取用户信息服务

--- user_module/go.mod
+++ user_module/go.mod
@@ -27,8 +27,11 @@ require (
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/genproto v0.0.0-20220505152158-f39f71e6c8f3 // indirect
+	google.golang.org/grpc v1.46.0
+	google.golang.org/protobuf v1.28.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- user_module/main.go
+++ user_module/main.go
@@ -2,27 +2,63 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"net/http"
+	"strings"
 	"sync"
 	"user_module/controller"
 	"user_module/dao"
+	"user_module/douyinRpc"
 )
 
 func main(){
 	//initial
 	waitGroup := sync.WaitGroup{}
-	waitGroup.Add(2)
+	waitGroup.Add(3)
 	go func() {
 		dao.InitialDB()
 		waitGroup.Done()
 	}()
 	r := gin.Default()
+	grpcServer := grpc.NewServer()
 	go func() {
 		InitRouter(r)
+		// global interceptor
+		r.Use(func(ctx *gin.Context) {
+			// is http/2
+			// is grpc
+			if ctx.Request.ProtoMajor == 2 &&
+				strings.HasPrefix(ctx.GetHeader("Content-Type"), "application/grpc") {
+				// 按grpc方式来请求
+				grpcServer.ServeHTTP(ctx.Writer, ctx.Request)
+				// 不要再往下请求了,防止继续链式调用拦截器
+				ctx.Abort()
+				return
+			}
+			//comon api
+			ctx.Next()
+		})
+		waitGroup.Done()
+	}()
+	go func() {
+		//initial rpc server
+		douyinRpc.RegisterRpcUserServiceServer(grpcServer,&douyinRpc.UserRpcServiceImpl{})
 		waitGroup.Done()
 	}()
 	waitGroup.Wait()
+
+	//initial http/2
+	h2Handle := h2c.NewHandler(r, &http2.Server{}) // 禁用TLS加密协议
+	//set up http service
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: h2Handle,
+	}
 	//start
-	r.Run()
+	server.ListenAndServe()
+
 }
 
 //初始化路由
